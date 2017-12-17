0d07f73f03380beaf9543cb33c7ea6ace8db1aae
Thomas Johnson <thomasej2000@hotmail.com>
2017-12-17 00:09:43 -0800

    add Rock oppenent stub

---
+++ robantu/opponents.py
@@ -0,0 +1,18 @@
+""" TODO """
+
+import arcade
+
+from .entities import Entity
+
+
+class Rock(Entity):
+
+    def __init__(self, x, y):
+        super().__init__(x, y)
+
+    def update(self):
+        pass
+
+    def draw():
+        pass
+    

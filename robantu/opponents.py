6e06166ab58091c89f66d804a0c392bb450e83e2
Thomas Johnson <thomasej2000@hotmail.com>
2017-12-17 00:16:42 -0800

    add oppenent class which is aware of current robantu

--- robantu/opponents.py
+++ robantu/opponents.py
@@ -5,10 +5,17 @@ import arcade
 from .entities import Entity
 
 
-class Rock(Entity):
+class Opponent(Entity):
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, robantu):
         super().__init__(x, y)
+        self.robantu = robantu
+
+
+class Rock(Opponent):
+
+    # def __init__(self, x, y):
+    #     super().__init__(x, y)
 
     def update(self):
         pass
